96fb654177bbef93ffed16f8d15fa8f83bb306ba
vitor013 <vitorrabelodaraujo013@gmail.com>
2024-01-08 15:53:00 -0300

    form v2

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        New Header
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .header {
+        background-color: rgb(130, 130, 175);
+    }
+</style>
